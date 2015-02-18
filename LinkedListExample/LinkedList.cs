6d172f9adfd1d92344244d549a92086a07bd5686
Marc Hartsuiker <marc.hartsuiker@gmail.com>
2015-02-18 20:21:04 +0100

    Added linked list code

---
+++ LinkedListExample/LinkedList.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkedListExample
+{
+    public class LinkedList<T>
+    {
+        private class Node
+        {
+            public Node next;
+            public T data;
+        }
+
+        private Node head = null;
+
+        public void AddNode(T data)
+        {
+            Node newNode = new Node();
+            newNode.next = head;
+            newNode.data = data;
+            head = newNode; 
+        }
+
+        public void PrintLinkedListItems()
+        {
+            Node currentNode = head;
+
+            while (currentNode != null)
+            {
+                Console.WriteLine(currentNode.data);
+                currentNode = currentNode.next;
+            }
+
+            Console.WriteLine("Press enter to exit.");
+            Console.ReadLine();
+        } 
+    }
+}
